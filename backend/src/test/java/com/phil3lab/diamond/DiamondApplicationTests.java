7aaccdf77c6d525be0e44f8862f37dd26f491d17
daniel-js01 <daniel.yc.ju@gmail.com>
2026-06-07 15:32:44 +0900

    feat: initialize frontend and backend project structure

---
+++ backend/src/test/java/com/phil3lab/diamond/DiamondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phil3lab.diamond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
